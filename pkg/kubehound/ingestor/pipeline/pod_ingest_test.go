2efe23e963a0f9cb8a824bbd05db989204b51261
Jeremy Fox <jeremy.fox@datadoghq.com>
2023-05-22 11:13:56 +0100

    WIP

---
+++ pkg/kubehound/ingestor/pipeline/pod_ingest_test.go
@@ -0,0 +1,92 @@
+package pipeline
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/DataDog/KubeHound/pkg/collector"
+	mockcollect "github.com/DataDog/KubeHound/pkg/collector/mocks"
+	"github.com/DataDog/KubeHound/pkg/globals/types"
+	cache "github.com/DataDog/KubeHound/pkg/kubehound/storage/cache/mocks"
+	graphdb "github.com/DataDog/KubeHound/pkg/kubehound/storage/graphdb/mocks"
+	storedb "github.com/DataDog/KubeHound/pkg/kubehound/storage/storedb/mocks"
+	"github.com/DataDog/KubeHound/pkg/kubehound/store/collections"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
+func TestPodIngest_Pipeline(t *testing.T) {
+	pi := &PodIngest{}
+
+	ctx := context.Background()
+	fakePod, err := loadTestObject[types.PodType]("testdata/pod.json")
+	assert.NoError(t, err)
+
+	client := mockcollect.NewCollectorClient(t)
+	client.EXPECT().StreamPods(ctx, mock.Anything, mock.Anything).
+		RunAndReturn(func(ctx context.Context, process collector.PodProcessor, complete collector.Complete) error {
+			// Fake the stream of a single pod from the collector client
+			err := process(ctx, fakePod)
+			if err != nil {
+				return err
+			}
+
+			return complete(ctx)
+		})
+
+	// Cache setup
+	c := cache.NewCacheProvider(t)
+	cw := cache.NewAsyncWriter(t)
+	cwDone := make(chan struct{})
+	cw.EXPECT().Queue(ctx, mock.AnythingOfType("*cache.roleCacheKey"), mock.AnythingOfType("primitive.ObjectID")).Return(nil).Once()
+	cw.EXPECT().Flush(ctx).Return(cwDone, nil)
+	cw.EXPECT().Close(ctx).Return(nil)
+	c.EXPECT().BulkWriter(ctx).Return(cw, nil)
+
+	// Store setup
+	sdb := storedb.NewProvider(t)
+	sw := storedb.NewAsyncWriter(t)
+	roles := collections.Pod{}
+	swDone := make(chan struct{})
+	sw.EXPECT().Queue(ctx, mock.AnythingOfType("*store.Pod")).Return(nil).Once()
+	sw.EXPECT().Flush(ctx).Return(swDone, nil)
+	sw.EXPECT().Close(ctx).Return(nil)
+	sdb.EXPECT().BulkWriter(ctx, roles).Return(sw, nil)
+
+	// Graph setup
+	gdb := graphdb.NewProvider(t)
+	gw := graphdb.NewAsyncVertexWriter(t)
+	gwDone := make(chan struct{})
+	gw.EXPECT().Queue(ctx, mock.AnythingOfType("*graph.Pod")).Return(nil).Once()
+	gw.EXPECT().Flush(ctx).Return(gwDone, nil)
+	gw.EXPECT().Close(mock.Anything).Return(nil)
+	gdb.EXPECT().VertexWriter(ctx, mock.AnythingOfType("vertex.VertexTraversal")).Return(gw, nil)
+
+	deps := &Dependencies{
+		Collector: client,
+		Cache:     c,
+		GraphDB:   gdb,
+		StoreDB:   sdb,
+	}
+
+	// Initialize
+	err = pi.Initialize(ctx, deps)
+	assert.NoError(t, err)
+
+	go func() {
+		// Simulate a delayed flush completion
+		time.Sleep(time.Second)
+		close(cwDone)
+		close(swDone)
+		close(gwDone)
+	}()
+
+	// Run
+	err = pi.Run(ctx)
+	assert.NoError(t, err)
+
+	// Close
+	err = pi.Close(ctx)
+	assert.NoError(t, err)
+}
