ec10c3d49b08c6ba37188f10113b66129cdd5af8
Jeremy Fox <jeremy.fox@datadoghq.com>
2023-05-24 13:22:57 +0100

    WIP

--- test/setup/create-cluster-resources.sh
+++ test/setup/create-cluster-resources.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
 
+CLUSTER_NAME=kubehound.test.local
+CONFIG_DIR=./test-cluster
+
 echo "[*] Deploying test resources via kubectl apply"
 kubectl apply -f "${CONFIG_DIR}/priv-pod.yaml" --context "kind-${CLUSTER_NAME}"
 kubectl apply -f "${CONFIG_DIR}/priv-pid-pod.yaml" --context "kind-${CLUSTER_NAME}"
