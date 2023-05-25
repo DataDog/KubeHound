9ceb144234e6ff43a5038fb45aced6c45f3368c6
Jeremy Fox <109584719+d0g0x01@users.noreply.github.com>
2023-05-25 11:41:11 +0100

    [CORE] config path argument (#21)

--- cmd/kubehound/root.go
+++ cmd/kubehound/root.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 
+	"github.com/DataDog/KubeHound/pkg/config"
 	"github.com/DataDog/KubeHound/pkg/kubehound/core"
 	"github.com/spf13/cobra"
 )
@@ -10,12 +11,20 @@ import (
 // TODO config file path
 
 var (
+	cfgFile = config.DefaultConfigPath
+)
+
+var (
 	rootCmd = &cobra.Command{
 		Use:   "kubehound-local",
 		Short: "A local Kubehound instance",
 		Long:  `A local instance of Kubehound - a Kubernetes attack path generator`,
 		RunE: func(cmd *cobra.Command, args []string) error {
-			return core.Launch(context.Background())
+			return core.Launch(context.Background(), core.WithConfigPath(cfgFile))
 		},
 	}
 )
+
+func init() {
+	rootCmd.PersistentFlags().StringVarP(&cfgFile, "config", "c", cfgFile, "application config file")
+}
