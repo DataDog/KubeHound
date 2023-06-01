1b4aa21c56b129c4a4a5059708e18d4620ec924c
Edouard Schweisguth <edouard.schweisguth@datadoghq.com>
2023-06-01 17:12:48 +0200

    Add statsd boilerplate

--- pkg/telemetry/telemetry.go
+++ pkg/telemetry/telemetry.go
@@ -1,19 +1,34 @@
 package telemetry
 
 import (
-	"github.com/DataDog/KubeHound/pkg/globals"
+	"github.com/DataDog/KubeHound/pkg/telemetry/log"
+	"github.com/DataDog/datadog-go/v5/statsd"
 )
 
-type TelemetryClient struct {
+// just to make sure we have a client that does nothing by default
+func init() {
+	statsdClient = &NoopClient{}
 }
 
-func Initialize() (*TelemetryClient, error) {
+func Initialize(statsdURL string) error {
+	var err error
+	// In case we don't have a statsd url set, we just want to continue, but log that we aren't going to submit metrics.
+	if statsdURL == "" {
+		log.I.Warn("No metrics collector has been setup. All metrics submission are going to be NOOP.")
+		return nil
+	}
+	statsdClient, err = statsd.New(statsdURL)
+	if err != nil {
+		return err
+	}
 	// Initialize all telemetry required
 	// return client to enable clean shutdown
-	return nil, globals.ErrNotImplemented
+	return nil
 }
 
-func (t *TelemetryClient) Shutdown() {
-	// Flush telemtry
-	// Close clients
+func Shutdown() {
+	err := statsdClient.Flush()
+	log.I.Warnf("Failed to flush statsd client: %v", err)
+	statsdClient.Close()
+	log.I.Warnf("Failed to close statsd client: %v", err)
 }
