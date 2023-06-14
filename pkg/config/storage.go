4bcd2f9b9cfdeeb8900255a5f3afe727670ab9ce
jt-dd <julien.terriac@datadoghq.com>
2023-06-14 19:22:21 +0200

    snake case in the mean time

--- pkg/config/storage.go
+++ pkg/config/storage.go
@@ -4,5 +4,5 @@ import "time"
 
 type StorageConfig struct {
 	Retry      int           `mapstructure:"retry"`
-	RetryDelay time.Duration `mapstructure:"retryDelay"`
+	RetryDelay time.Duration `mapstructure:"retry_delay"`
 }
