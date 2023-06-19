9c5f18e7d76dcf9a8baf43ad83b7c607aee27191
Edouard Schweisguth <edouard.schweisguth@datadoghq.com>
2023-06-19 17:33:38 +0200

    Updates

--- deployments/kubehound/start.sh
+++ deployments/kubehound/start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 
-# Spin up the compose with neo4j, mongodb, etc
+# Spin up the compose with janusgraph, mongodb, datadog, etc
 docker-compose -f ./docker-compose-dev.yaml up --force-recreate --build
