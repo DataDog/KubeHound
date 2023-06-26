d8138a546f86877be4a239e33534735585b54fae
Jeremy Fox <jeremy.fox@datadoghq.com>
2023-06-26 20:16:54 +0100

    WIP

--- pkg/kubehound/risk/engine.go
+++ pkg/kubehound/risk/engine.go
@@ -10,10 +10,11 @@ import (
 var engineInstance *RiskEngine
 var riOnce sync.Once
 
+// Engine returns the risk engine singleton instance.
 func Engine() *RiskEngine {
 	var err error
 	riOnce.Do(func() {
-		engineInstance, err = NewEngine()
+		engineInstance, err = newEngine()
 		if err != nil {
 			log.I.Fatalf("Risk engine initialization: %v", err)
 		}
@@ -22,16 +23,20 @@ func Engine() *RiskEngine {
 	return engineInstance
 }
 
+// RiskEngine computes which assets are deemed critical based on a set of pre-configured rules.
 type RiskEngine struct {
 	roleMap map[string]bool // Map of critical roles
 }
 
-func NewEngine() (*RiskEngine, error) {
+// newEngine creates a new risk engine instance. Should not be called directly.
+func newEngine() (*RiskEngine, error) {
 	return &RiskEngine{
 		roleMap: CriticalRoleMap,
 	}, nil
 }
 
+// IsCritical reports whether the provided asset should be marked as critical.
+// The function expects a single store model input and currently only supports Roles.
 func (ra *RiskEngine) IsCritical(model any) bool {
 	switch o := model.(type) {
 	case *store.Role:
