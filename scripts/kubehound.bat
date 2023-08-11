c8e162a8312033bbd75f3a75dcaa2b026c1e57aa
Jeremy Fox <109584719+d0g0x01@users.noreply.github.com>
2023-08-11 14:49:31 +0100

    [CORE] Add binary releases (#91)

---
+++ scripts/kubehound.bat
@@ -0,0 +1,54 @@
+@echo off
+
+REM Lightweight wrapper script to run KubeHound from a release archive
+
+set KUBEHOUND_ENV=release
+set DOCKER_CMD=docker
+set DOCKER_COMPOSE_FILE_PATH=-f deployments\kubehound\docker-compose.yaml
+set DOCKER_COMPOSE_FILE_PATH=%DOCKER_COMPOSE_FILE_PATH% -f deployments\kubehound\docker-compose.release.yaml
+if not "%DD_API_KEY%"=="" (
+    set DOCKER_COMPOSE_FILE_PATH=%DOCKER_COMPOSE_FILE_PATH% -f deployments\kubehound\docker-compose.datadog.yaml
+)
+
+set DOCKER_COMPOSE_PROFILE=--profile infra
+
+:run
+ ./kubehound -c config.yaml
+goto :eof
+
+:backend-down
+%DOCKER_CMD% compose %DOCKER_COMPOSE_FILE_PATH% %DOCKER_COMPOSE_PROFILE% rm -fvs
+goto :eof
+
+:backend-up
+%DOCKER_CMD% compose %DOCKER_COMPOSE_FILE_PATH% %DOCKER_COMPOSE_PROFILE% up --force-recreate --build -d
+goto :eof
+
+:backend-reset
+%DOCKER_CMD% compose %DOCKER_COMPOSE_FILE_PATH% %DOCKER_COMPOSE_PROFILE% rm -fvs
+%DOCKER_CMD% compose %DOCKER_COMPOSE_FILE_PATH% %DOCKER_COMPOSE_PROFILE% up --force-recreate --build -d
+goto :eof
+
+:backend-reset-hard
+call :backend-down
+%DOCKER_CMD% volume rm kubehound-%KUBEHOUND_ENV%_mongodb_data
+%DOCKER_CMD% volume rm kubehound-%KUBEHOUND_ENV%_janusgraph_data
+call :backend-up
+goto :eof
+
+if "%1"=="" (
+    echo Usage: %0 {run^|backend-up^|backend-reset^|backend-reset-hard^|backend-down}
+    exit /b 1
+)
+
+setlocal enabledelayedexpansion
+for %%i in (run backend-up backend-reset backend-reset-hard backend-down) do (
+    if "%%i"=="%1" (
+        endlocal
+        goto %%i
+    )
+)
+endlocal
+
+echo Usage: %0 {run^|backend-up^|backend-reset^|backend-reset-hard^|backend-down}
+exit /b 1
