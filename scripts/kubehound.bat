e5fbf8ec5a326040f21f5e79a8dfe7a314751134
Jeremy Fox <jeremy.fox@datadoghq.com>
2023-08-11 11:10:03 +0100

    WIP

--- scripts/kubehound.bat
+++ scripts/kubehound.bat
@@ -1,1 +1,51 @@
-TODO TODO
+@echo off
+
+set KUBEHOUND_ENV=release
+set DOCKER_CMD=docker
+set DOCKER_COMPOSE_FILE_PATH=-f deployments\kubehound\docker-compose.yaml
+set DOCKER_COMPOSE_FILE_PATH=%DOCKER_COMPOSE_FILE_PATH% -f deployments\kubehound\docker-compose.release.yaml
+if not "%DD_API_KEY%"=="" (
+    set DOCKER_COMPOSE_FILE_PATH=%DOCKER_COMPOSE_FILE_PATH% -f deployments\kubehound\docker-compose.datadog.yaml
+)
+
+set DOCKER_COMPOSE_PROFILE=--profile infra
+
+:run
+REM TODO: run kubehound with config file
+goto :eof
+
+:backend-down
+%DOCKER_CMD% compose %DOCKER_COMPOSE_FILE_PATH% %DOCKER_COMPOSE_PROFILE% rm -fvs
+goto :eof
+
+:backend-up
+%DOCKER_CMD% compose %DOCKER_COMPOSE_FILE_PATH% %DOCKER_COMPOSE_PROFILE% up --force-recreate --build -d
+goto :eof
+
+:backend-reset
+%DOCKER_CMD% compose %DOCKER_COMPOSE_FILE_PATH% %DOCKER_COMPOSE_PROFILE% rm -fvs
+%DOCKER_CMD% compose %DOCKER_COMPOSE_FILE_PATH% %DOCKER_COMPOSE_PROFILE% up --force-recreate --build -d
+goto :eof
+
+:backend-reset-hard
+%DOCKER_CMD% volume rm kubehound-%KUBEHOUND_ENV%_mongodb_data
+%DOCKER_CMD% volume rm kubehound-%KUBEHOUND_ENV%_janusgraph_data
+call :backend-reset
+goto :eof
+
+if "%1"=="" (
+    echo Usage: %0 {run^|backend-up^|backend-reset^|backend-reset-hard^|backend-down}
+    exit /b 1
+)
+
+setlocal enabledelayedexpansion
+for %%i in (run backend-up backend-reset backend-reset-hard backend-down) do (
+    if "%%i"=="%1" (
+        endlocal
+        goto %%i
+    )
+)
+endlocal
+
+echo Usage: %0 {run^|backend-up^|backend-reset^|backend-reset-hard^|backend-down}
+exit /b 1
--- scripts/kubehound.sh
+++ scripts/kubehound.sh
@@ -1,5 +1,73 @@
-copy
-backend-reset
-backend-reset-hard
-backendup
-runm
+#!/bin/bash
+
+KUBEHOUND_ENV="release"
+DOCKER_COMPOSE_FILE_PATH="-f deployments/kubehound/docker-compose.yaml"
+DOCKER_COMPOSE_FILE_PATH+=" -f deployments/kubehound/docker-compose.release.yaml"
+if [ -n "${DD_API_KEY}" ]; then
+    DOCKER_COMPOSE_FILE_PATH+=" -f deployments/kubehound/docker-compose.datadog.yaml"
+fi
+
+DOCKER_COMPOSE_PROFILE="--profile infra"
+
+UNAME_S=$(uname -s)
+if [ -z "${DOCKER_CMD}" ]; then
+    if [ "${UNAME_S}" == "Linux" ]; then
+        if [ -z "${CI}" ]; then
+            DOCKER_CMD="sudo docker"
+        else
+            DOCKER_CMD="docker"
+        fi
+    else
+        DOCKER_CMD="docker"
+    fi
+    DOCKER_CMD="${DOCKER_CMD}"
+fi
+
+DOCKER_HOSTNAME=$(hostname)
+if [ -z "${CI}" ]; then
+    DOCKER_CMD="DOCKER_HOSTNAME=${DOCKER_HOSTNAME} ${DOCKER_CMD}"
+fi
+
+run() {
+    # TODO run kubehound with config file
+}
+
+backend_down() {
+    ${DOCKER_CMD} compose ${DOCKER_COMPOSE_FILE_PATH} ${DOCKER_COMPOSE_PROFILE} rm -fvs
+}
+
+backend_up() {
+    ${DOCKER_CMD} compose ${DOCKER_COMPOSE_FILE_PATH} ${DOCKER_COMPOSE_PROFILE} up --force-recreate --build -d
+}
+
+backend_reset() {
+    ${DOCKER_CMD} compose ${DOCKER_COMPOSE_FILE_PATH} ${DOCKER_COMPOSE_PROFILE} rm -fvs
+    ${DOCKER_CMD} compose ${DOCKER_COMPOSE_FILE_PATH} ${DOCKER_COMPOSE_PROFILE} up --force-recreate --build -d
+}
+
+backend_reset_hard() {
+    ${DOCKER_CMD} volume rm kubehound-${KUBEHOUND_ENV}_mongodb_data
+    ${DOCKER_CMD} volume rm kubehound-${KUBEHOUND_ENV}_janusgraph_data
+    backend_reset()
+}
+
+case "$1" in
+    run)
+        run
+        ;;
+    backend-down)
+        backend_down
+        ;;
+    backend-up)
+        backend_up
+        ;;
+    backend-reset)
+        backend_reset
+        ;;
+    backend-reset-hard)
+        backend_reset_hard
+        ;;
+    *)
+        echo "Usage: $0 {run|backend-up|backend-reset|backend-reset-hard|backend-down}"
+        exit 1
+esac
