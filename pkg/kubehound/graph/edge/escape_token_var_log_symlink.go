43d89d04d86e5b9c6175c0f0abb94a31703f2f3d
Edouard Schweisguth <edouard.schweisguth@datadoghq.com>
2023-09-19 12:39:56 +0200

    WIP

--- pkg/kubehound/graph/edge/escape_token_var_log_symlink.go
+++ pkg/kubehound/graph/edge/escape_token_var_log_symlink.go
@@ -64,7 +64,7 @@ func (e *EscapeTokenVarLogSymlink) Traversal() types.EdgeTraversal {
 			InE("IDENTITY_ASSUME").OutV().
 			HasLabel("Container").As("c").OutE("VOLUME_DISCOVER").
 			Has("sourcePath", P.Within("/", "/var", "/var/log")).
-			AddE().
+			AddE(e.Label()).
 			From("c").To(inserts)
 		return g
 	}
@@ -73,131 +73,36 @@ func (e *EscapeTokenVarLogSymlink) Traversal() types.EdgeTraversal {
 func (e *EscapeTokenVarLogSymlink) Stream(ctx context.Context, store storedb.Provider, _ cache.CacheReader,
 	callback types.ProcessEntryCallback, complete types.CompleteQueryCallback) error {
 
-	volumes := adapter.MongoDB(store).Collection(collections.VolumeName)
-
-	// Container.volumeMounts[*].hostPath.path contains /var/log
-	// Container.volumeMounts[*].hostPath.readOnly is false
-	// Container.securityContext.runAsUser is 0
-	pipeline := bson.A{
-		bson.D{
-			{Key: "$match",
-				Value: bson.D{
-					{Key: "$and",
-						Value: bson.A{
-							bson.D{
-								{Key: "$or",
-									Value: bson.A{
-										bson.D{{Key: "source", Value: "/var/log"}},
-										bson.D{{Key: "source", Value: "/var"}},
-										bson.D{{Key: "source", Value: "/"}},
-									},
-								},
-							},
-							bson.D{{Key: "readonly", Value: false}},
+	permissionSets := adapter.MongoDB(store).Collection(collections.PermissionSetName)
+	pipeline := []bson.M{
+		{
+			"$match": bson.M{
+				"rules": bson.M{
+					"$elemMatch": bson.M{
+						"$and": bson.A{
+							bson.M{"$or": bson.A{
+								bson.M{"resources": "pods/log"},
+								bson.M{"resources": "pods/*"},
+								bson.M{"resources": "*"},
+							}},
+							bson.M{"$or": bson.A{
+								bson.M{"verbs": "get"},
+								bson.M{"verbs": "*"},
+							}},
+							bson.M{"resourcenames": nil}, // TODO: handle resource scope
 						},
 					},
 				},
 			},
 		},
-		bson.D{
-			{Key: "$lookup",
-				Value: bson.D{
-					{Key: "from", Value: "containers"},
-					{Key: "localField", Value: "container_id"},
-					{Key: "foreignField", Value: "_id"},
-					{Key: "as", Value: "containers"},
-				},
-			},
-		},
-		bson.D{
-			{Key: "$unwind",
-				Value: bson.D{
-					{Key: "path", Value: "$containers"},
-					{Key: "preserveNullAndEmptyArrays", Value: false},
-				},
-			},
-		},
-		bson.D{
-			{Key: "$lookup",
-				Value: bson.D{
-					{Key: "from", Value: "identities"},
-					{Key: "localField", Value: "containers.inherited.service_account"},
-					{Key: "foreignField", Value: "name"},
-					{Key: "as", Value: "service_account"},
-				},
-			},
-		},
-		bson.D{
-			{Key: "$lookup",
-				Value: bson.D{
-					{Key: "from", Value: "rolebindings"},
-					{Key: "localField", Value: "service_account.name"},
-					{Key: "foreignField", Value: "subjects.subject.name"},
-					{Key: "as", Value: "rolebindings"},
-				},
-			},
-		},
-		bson.D{
-			{Key: "$unwind",
-				Value: bson.D{
-					{Key: "path", Value: "$rolebindings"},
-					{Key: "preserveNullAndEmptyArrays", Value: false},
-				},
-			},
-		},
-		bson.D{
-			{Key: "$lookup",
-				Value: bson.D{
-					{Key: "from", Value: "permissionsets"},
-					{Key: "localField", Value: "rolebindings.role_id"},
-					{Key: "foreignField", Value: "role_id"},
-					{Key: "as", Value: "perms"},
-				},
-			},
-		},
-		bson.D{
-			{Key: "$match",
-				Value: bson.D{
-					{Key: "$and",
-						Value: bson.A{
-							bson.D{
-								{Key: "perms.rules.verbs",
-									Value: bson.D{
-										{Key: "$in",
-											Value: bson.A{
-												"get",
-											},
-										},
-									},
-								},
-							},
-							bson.D{
-								{Key: "perms.rules.resources",
-									Value: bson.D{
-										{Key: "$in",
-											Value: bson.A{
-												"pods/log",
-											},
-										},
-									},
-								},
-							},
-						},
-					},
-				},
-			},
-		},
-		bson.D{
-			{Key: "$project",
-				Value: bson.D{
-					{Key: "node_id", Value: 1},
-					{Key: "container_id", Value: 1},
-				},
+		{
+			"$project": bson.M{
+				"_id": 1,
 			},
 		},
 	}
 
-	cur, err := volumes.Aggregate(context.Background(), pipeline)
+	cur, err := permissionSets.Aggregate(context.Background(), pipeline)
 	if err != nil {
 		return err
 	}
