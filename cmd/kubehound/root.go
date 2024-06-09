38cdabb8cd720bc807baffd418f9b258f425644c
jt-dd <julien.terriac@datadoghq.com>
2024-06-09 12:22:34 +0200

    own command for version

--- cmd/kubehound/root.go
+++ cmd/kubehound/root.go
@@ -2,11 +2,9 @@ package main
 
 import (
 	"fmt"
-	"os"
 
 	"github.com/DataDog/KubeHound/pkg/backend"
 	"github.com/DataDog/KubeHound/pkg/cmd"
-	"github.com/DataDog/KubeHound/pkg/config"
 	"github.com/DataDog/KubeHound/pkg/kubehound/core"
 	"github.com/DataDog/KubeHound/pkg/telemetry/log"
 	"github.com/spf13/cobra"
@@ -24,11 +22,6 @@ var (
 		Short: "A local Kubehound instance",
 		Long:  `A local instance of Kubehound - a Kubernetes attack path generator`,
 		PreRunE: func(cobraCmd *cobra.Command, args []string) error {
-			if printVersion {
-				log.I.Infof("kubehound version: %s", config.BuildVersion)
-				os.Exit(0)
-			}
-
 			return cmd.InitializeKubehoundConfig(cobraCmd.Context(), cfgFile, true, false)
 		},
 		RunE: func(cobraCmd *cobra.Command, args []string) error {
@@ -68,8 +61,6 @@ var (
 )
 
 func init() {
-	rootCmd.Flags().BoolVarP(&printVersion, "version", "v", printVersion, "print the current version of Kubehound")
-
 	rootCmd.Flags().StringVarP(&cfgFile, "config", "c", cfgFile, "application config file")
 
 	rootCmd.Flags().BoolVar(&skipBackend, "skip-backend", skipBackend, "skip the auto deployment of the backend stack (janusgraph, mongodb, and UI)")
---
+++ cmd/kubehound/version.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"github.com/DataDog/KubeHound/pkg/config"
+	"github.com/DataDog/KubeHound/pkg/telemetry/log"
+	"github.com/spf13/cobra"
+)
+
+var (
+	versionCmd = &cobra.Command{
+		Use:   "version",
+		Short: "print Kubehound version",
+		Long:  `print the current version of Kubehound`,
+		Run: func(cobraCmd *cobra.Command, args []string) {
+			log.I.Infof("kubehound version: %s %s/%s", config.BuildVersion, config.BuildArch, config.BuildOs)
+		},
+	}
+)
+
+func init() {
+	rootCmd.AddCommand(versionCmd)
+}
