821bea363bf642273ef12a1b960917f33b24b295
jt-dd <112463504+jt-dd@users.noreply.github.com>
2024-09-16 10:05:05 +0200

    Rename remote bucket flag to bucket-url (#267)

--- pkg/cmd/dump.go
+++ pkg/cmd/dump.go
@@ -37,8 +37,8 @@ func InitLocalDumpCmd(cmd *cobra.Command) {
 }
 
 func InitRemoteDumpCmd(cmd *cobra.Command) {
-	cmd.Flags().String("bucket", "", "Bucket to use to push k8s resources (e.g.: s3://<your_bucket>)")
-	viper.BindPFlag(config.CollectorFileBlobBucket, cmd.Flags().Lookup("bucket")) //nolint: errcheck
+	cmd.Flags().String("bucket-url", "", "Bucket to use to push k8s resources (e.g.: s3://<your_bucket>)")
+	viper.BindPFlag(config.CollectorFileBlobBucket, cmd.Flags().Lookup("bucket-url")) //nolint: errcheck
 
 	cmd.Flags().String("region", "", "Region to retrieve the configuration (only for s3) (e.g.: us-east-1)")
 	viper.BindPFlag(config.CollectorFileBlobRegion, cmd.Flags().Lookup("region")) //nolint: errcheck
