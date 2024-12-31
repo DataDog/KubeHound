798ef8e260373b9b07a7e3ee94288ae853737537
Simon Maréchal <simon.marechal@datadoghq.com>
2024-12-31 09:08:44 +0100

    typo: s/deamonsets/daemonsets/g

--- pkg/kubehound/graph/edge/pod_create.go
+++ pkg/kubehound/graph/edge/pod_create.go
@@ -121,7 +121,7 @@ func (e *PodCreate) Stream(ctx context.Context, store storedb.Provider, _ cache.
 							bson.M{"$or": bson.A{
 								bson.M{"resources": "pods"},
 								bson.M{"resources": "cronjobs"},
-								bson.M{"resources": "deamonsets"},
+								bson.M{"resources": "daemonsets"},
 								bson.M{"resources": "deployments"},
 								bson.M{"resources": "jobs"},
 								bson.M{"resources": "replicasets"},
