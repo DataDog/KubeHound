5595a742c478a0e13157d7e83ba6cf9026fbe61b
Simon Maréchal <simon.marechal@datadoghq.com>
2025-09-18 10:36:53 +0200

    Fix mongo clean function

--- pkg/kubehound/storage/storedb/mongo_provider.go
+++ pkg/kubehound/storage/storedb/mongo_provider.go
@@ -111,7 +111,7 @@ func (mp *MongoProvider) Prepare(ctx context.Context) error {
 	return nil
 }
 
-func (mp *MongoProvider) Clean(ctx context.Context, cluster string, runId string) error {
+func (mp *MongoProvider) Clean(ctx context.Context, runId string, clusterName string) error {
 	l := log.Logger(ctx)
 	db := mp.writer.Database(MongoDatabaseName)
 	collections, err := db.ListCollectionNames(ctx, bson.M{})
@@ -120,7 +120,7 @@ func (mp *MongoProvider) Clean(ctx context.Context, cluster string, runId string
 	}
 	filter := bson.M{
 		"runtime.runID":        runId,
-		"runtime.cluster.name": cluster,
+		"runtime.cluster.name": clusterName,
 	}
 	for _, collectionName := range collections {
 		res, err := db.Collection(collectionName).DeleteMany(ctx, filter)
